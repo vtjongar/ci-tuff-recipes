fe2e127bb4e3a2eb4867a5619456b68dff1d2cdc
Vanessa Henderson <bookvanny@gmail.com>
2019-06-11 17:52:00 -0400

    changed default rendering recipes

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ const App = () => {
 
   const [recipes, setRecipes] = useState([]);
   const [search, setSearch] = useState("");
-  const [query, setQuery] = useState("cheese");
+  const [query, setQuery] = useState("pie");
 
   useEffect(() => {
     getRecipes();
