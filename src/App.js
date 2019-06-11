0369d89f09a4d606becc8337ec741bb5d92943f6
Vanessa Henderson <bookvanny@gmail.com>
2019-06-11 17:54:59 -0400

    edited home page

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ const App = () => {
 
   const [recipes, setRecipes] = useState([]);
   const [search, setSearch] = useState("");
-  const [query, setQuery] = useState("pie");
+  const [query, setQuery] = useState("cheese");
 
   useEffect(() => {
     getRecipes();
